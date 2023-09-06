4e2414eb6622ae068d7269f610d1aa324e1ddb9c
wal72thela <walt2thela@gmail.com>
2023-09-06 23:14:30 +0200

    attempt on adding MYSQL database

--- DrPatient/PatientSide/models.py
+++ DrPatient/PatientSide/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=64)
+    age = models.IntegerField()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hacknosis
 Hackerthon medical project.
+
+To Run PatientSide:
+1. on your powershell, run DoctorPatient/Scripts/Activate.ps1 >> cd .\DrPatient\ >> python manage.py runserver
+
